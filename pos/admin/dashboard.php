63f78609e013ae0aadabc1f35d8328354d2ce363
NetGo+ Dev <36708000+rickxy@users.noreply.github.com>
2023-02-08 23:59:02 +0300

    Update dashboard.php

--- pos/admin/dashboard.php
+++ pos/admin/dashboard.php
@@ -20,6 +20,7 @@ require_once('partials/_analytics.php');
     <?php
     require_once('partials/_topnav.php');
     ?>
+	  
     <!-- Header -->
     <div style="background-image: url(assets/img/theme/restro00.jpg); background-size: cover;" class="header  pb-8 pt-5 pt-md-8">
       <span class="mask bg-gradient-dark opacity-8"></span>
